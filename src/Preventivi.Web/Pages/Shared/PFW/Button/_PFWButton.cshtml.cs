64127dacc300edb6efc224844ed5674e588c32c8
akosir <alfonsokosir@gmail.com>
2026-08-21 11:54:40 +0200

    refactor(pfw): completato button

---
+++ src/Preventivi.Web/Pages/Shared/PFW/Button/_PFWButton.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Preventivi.Web.Pages.Shared.Button
+{
+    public class _PFWButtonModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/Preventivi.Web/UI/Button/PFWButtonModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Preventivi.Web.UI.Button;
+
+public sealed class PFWButtonModel
+{
+    public string Testo { get; init; } = "";
+
+    public string? Icona { get; init; }
+
+    public string? Url { get; init; }
+
+    public string? OnClick { get; init; }
+
+    public string Variante { get; init; } = "primary";
+
+    public bool Abilitato { get; init; } = true;
+
+    public bool Visibile { get; init; } = true;
+}
